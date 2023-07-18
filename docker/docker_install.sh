e41290a7a92c5fc0ab5ea2512305fc4795050806
rosenthalpreston <129431344+rosenthalpreston@users.noreply.github.com>
2023-07-18 12:25:02 +0200

    Update docker_install.sh
    add: 
    - install of git as needed eventually 
    - improve comment script by sous - script . this is an execution of a script in parent script

--- docker/docker_install.sh
+++ docker/docker_install.sh
@@ -1,13 +1,14 @@
 #POURQUOI CE SCRIPT ?
 # =====> Pour avoir systèmatiquement la dernière version de docker stable
 
-#Installation de curl si necessaire
+#Installation de pre requis git & curl si necessaire
 sudo apt install curl -y
+sudo apt install git -y
 
-#telechargement du script d'installation docker latest
+#telechargement du sous-script d'installation docker latest depuis docker.com
 sudo curl -fsSL https://get.docker.com -o install-docker.sh
 
-#Execution du script d'installation docker
+#Execution du sous-script d'installation de docker
 sudo sh ./install-docker.sh
 
 #Ajout de l'user courant dans le groupe docker
